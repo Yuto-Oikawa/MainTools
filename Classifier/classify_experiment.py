60ab5b0edd83be96f710a1a9d927621c3d43c197
Yuto-Oikawa <84612396+Yuto-Oikawa@users.noreply.github.com>
2021-12-27 16:05:18 +0900

    Combining functions

--- Classifier/classify_experiment.py
+++ Classifier/classify_experiment.py
@@ -3,119 +3,165 @@ import os
 import sys
 args = sys.argv
 
+import shutil
 import pandas as pd
 from sklearn.svm import LinearSVC
 from sklearn.utils import all_estimators
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import cross_validate, StratifiedKFold, train_test_split
+import spacy
+nlp = spacy.load('ja_ginza')
 
 vectorizer = TfidfVectorizer(token_pattern='(?u)\\b\\w+\\b')
-target_names = [ '1.5次情報', '1次情報', '2次情報']
-df = pd.read_excel('台風豪雨福島_tokenized.xlsx')
+target_names = ['NOT','FOUND']
 #df = pd.read_excel(args[2])
+df = pd.read_csv(args[2], sep='\t')
+OUTPUT_DIR = 'result/'        
 
 
-def createTrainData_byDirectoryText():
 
-    dir = "train/"
-    groups = [f for f in os.listdir(dir)]
-    train_all  = []
-    length = []
-
-    for group in groups:
-        lines = open(dir+str(group)).readlines()
-        data = [line for line in lines]
-        train_all.extend(data)
-        length.append(len(data))
-
-    sesquiary = length[0]
-    primary = length[1]
-    secondary = length[2]
-
-    x_train = vectorizer.fit_transform(train_all)
-    y_train = [0]*sesquiary + [1]*primary + [2]*secondary
-
-    return x_train, y_train
+def tokenize(x_train):
+    result = []
+    
+    for line in x_train:
+        doc = nlp.tokenizer(line)
+        
+        sentence_tokenize = ''
+        for token in doc:
+            sentence_tokenize += token.orth_+'\n'
+            
+        result.append(sentence_tokenize)
+        
+    return result
 
 
-def createTrainData_byExcel():
+def create_train_data(tokenizer=True):
 
-    x_train = df.Text.values.tolist()
+    x_train = df.sentence.values.tolist()
+    if tokenizer:
+        x_train = tokenize(x_train)
     x_train = vectorizer.fit_transform(x_train)
     y_train = df.label.values.tolist()
-    groups = df.type.values.tolist()
 
     return x_train, y_train
 
 
-def createTrainTestData_byExcel():
-    train_df, test_df = train_test_split(df, test_size=0.2, shuffle=True, random_state=123, stratify=df[['label','type']])
+def create_train_test_data(tokenizer=True):
+    train_df, test_df = train_test_split(df, test_size=0.2, shuffle=True, random_state=123, stratify=df['label'])
     
-    x_train = train_df.Text.values.tolist()
-    x_train = vectorizer.fit_transform(x_train)
-    x_test = test_df.Text.values.tolist()
+    x_train = train_df.sentence.values.tolist()
+    x_test_raw = test_df.sentence.values.tolist()
+    
+    if tokenizer:
+        x_train = tokenize(x_train)
+        x_train = vectorizer.fit_transform(x_train)
+        x_test = tokenize(x_test_raw)
+        x_test = vectorizer.transform(x_test)
+    
+    else:
+        x_train = vectorizer.fit_transform(x_train)
+        x_test = vectorizer.transform(x_test_raw)
+
 
     y_train = train_df.label.values.tolist()
     y_test = test_df.label.values.tolist()
 
-    return x_train, y_train, x_test, y_test
+    return x_train, y_train, x_test, x_test_raw, y_test
 
 
 
-def HoldOut_validation(x_train, y_train, x_test, y_test):
+def HoldOut_validation(x_train, y_train, x_test, x_test_raw, y_test, clf=LinearSVC(), output=True):
     
-    clf = LinearSVC()
+    clf = clf
     clf.fit(x_train, y_train)
-    X_test = vectorizer.transform(x_test)
-    predict = clf.predict(X_test)
+    predict = clf.predict(x_test)
 
     df = pd.DataFrame()
-    df['Text'] = x_test
-    df['label'] = predict
-    df.to_csv('result.csv', index=False)
+    df['sentence'] = x_test_raw
+    df['true'] = y_test
+    df['predict'] = predict
 
-    print(args[1])
-    print()
-    print(confusion_matrix(y_test, predict))
-    print(classification_report(y_test, predict, target_names=target_names))
+    if output:
+        df.to_csv(f'result_{clf}.csv', index=False)
+        print(confusion_matrix(y_test, predict))
+        print(classification_report(y_test, predict, target_names=target_names))
+        
+    else:
+        df.to_csv(f'{OUTPUT_DIR}{clf}.csv', index=False)
 
 
-def KFold_validation(x_train, y_train):
+
+
+def KFold_validation(x_train, y_train, clf=LinearSVC(), output=True):
+    clf = clf
     score_funcs = ['accuracy','precision_macro','recall_macro','f1_macro',]
-    #scores = cross_validate(LinearSVC(), x_train, y_train, cv=GroupKFold(n_splits=3), groups = groups, scoring = score_funcs)
-    scores = cross_validate(LinearSVC(), x_train, y_train, cv=StratifiedKFold(n_splits=10), scoring = score_funcs)
-
-    print()
-    print('accuracy:', scores['test_accuracy'].mean())
-    print('precision:', scores['test_precision_macro'].mean())
-    print('recall:', scores['test_recall_macro'].mean())
-    print('f1:', scores['test_f1_macro'].mean())
-    print()
+    scores = cross_validate(clf, x_train, y_train, cv=StratifiedKFold(n_splits=10), scoring = score_funcs)
 
     # with open('voc.txt', 'w') as f:
     #     f.write('\n'.join(vectorizer.vocabulary_.keys()))
 
+    if output:
+        print(f'classifier  :{clf}')
+        print('accuracy     :{:.2f}'.format(scores['test_accuracy'].mean()))
+        print('precision    :{:.2f}'.format(scores['test_precision_macro'].mean()))
+        print('recall       :{:.2f}'.format(scores['test_recall_macro'].mean()))
+        print('f1           :{:.2f}'.format(scores['test_f1_macro'].mean()))
+
+    else:
+        print('10Fold_Validation_f1                   :{:.2f}'.format(scores['test_f1_macro'].mean()))
+        return scores['test_f1_macro'].mean()
 
 
-def Classifier_Comparison(x_train, y_train, x_test, y_test):
-    x_test = vectorizer.transform(x_test)
+def classifier_comparison(x_train, y_train, x_test, y_test, output=True, validation=True):
+
+    ranking = {}
+    
+    if output:
+        try:
+            os.makedirs(OUTPUT_DIR)
+        except FileExistsError:
+            # フォルダの中身を全て削除してから新規作成
+            shutil.rmtree(OUTPUT_DIR)
+            os.makedirs(OUTPUT_DIR)
+
 
     for name, Estimator in all_estimators(type_filter="classifier"):
-        if name in {'CheckingClassifier', 'ClassifierChain', 'MultiOutputClassifier', 'OneVsOneClassifier', 'OneVsRestClassifier', 'OutputCodeClassifier', 'VotingClassifier', 'LogisticRegressionCV'} :
+        if name in {'CheckingClassifier', 'ClassifierChain', 'MultiOutputClassifier', 'OneVsOneClassifier', 'OneVsRestClassifier', 'OutputCodeClassifier', 'VotingClassifier', 'StackingClassifier','LogisticRegressionCV'} :
             continue
         model = Estimator()
 
         try:
-            model.fit(x_train,y_train)
-            predict = model.predict(x_test)
             print()
             print()
             print(name)
+            model.fit(x_train,y_train)
+            predict = model.predict(x_test)
+            print()
             print(confusion_matrix(y_test, predict))
             print(classification_report(y_test, predict, digits = 2,target_names=target_names))
-        except:
+            
+            HoldOut_validation(x_train, y_train, x_test, x_test_raw, y_test, clf=model, output=False)
+
+            if validation:
+                f1 = KFold_validation(x_train,y_train, clf=model, output=False)
+                ranking[name] = round(f1, 3)
+                
+            else:
+                result = classification_report(y_test, predict, digits = 2,target_names=target_names, output_dict=True)
+                f1 = result['macro avg']['f1-score']
+                ranking[name] = round(f1, 3)
+                
+            
+        except TypeError:
             pass
+        except ValueError:
+            pass
+    
+    print()
+    ranking =  sorted(ranking.items(), key=lambda i: i[1], reverse=True)
+    for element in ranking:
+        print(element)
 
 
 def print_usage():
@@ -124,27 +170,23 @@ def print_usage():
     print()
     print('1: HoldOut_validation')
     print('2: KFold_validation')
-    print('3: Classifier_Comparison')
+    print('3: classifier_comparison')
     print()   
+    
 
 if __name__ == '__main__':
     
-    x_train, y_train, x_test, y_test = createTrainTestData_byExcel()
+    if args[1] == '1':
+        x_train, y_train, x_test, x_test_raw, y_test = create_train_test_data(tokenizer=True)
+        HoldOut_validation(x_train, y_train, x_test, x_test_raw, y_test)
 
-    try:
-        if args[1] == '1':
-            HoldOut_validation(x_train, y_train, x_test, y_test)
-
-        elif args[1] =='2':
-            #x_train, y_train = createTrainData_byDirectoryText()
-            x_train, y_train = createTrainData_byExcel()
-            KFold_validation(x_train,y_train)
-        
-        elif args[1] =='3':
-            Classifier_Comparison(x_train, y_train, x_test, y_test)
-            
-        else:
-            print_usage()
+    elif args[1] =='2':
+        x_train, y_train = create_train_data(tokenizer=True)
+        KFold_validation(x_train,y_train)
     
-    except:
+    elif args[1] =='3':
+        x_train, y_train, x_test, x_test_raw, y_test = create_train_test_data(tokenizer=True)
+        classifier_comparison(x_train, y_train, x_test, y_test, output=True, validation=True)
+        
+    else:
         print_usage()
